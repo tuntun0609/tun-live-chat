2f4a948d3f43ff1c6ced5fa023f9023859392236
tuntun <760523867@qq.com>
2022-12-31 07:09:11 +0800

    style: 修改代码样式

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # tun-live-chat
-网页版哔哩哔哩直播姬
+网页版哔哩哔哩直播姬 | view the live danmu on the web page
--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,13 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@utils': path.resolve(__dirname, "src/utils"),
-      '@components': path.resolve(__dirname, "src/components"),
-      antd: path.join(__dirname, 'node_modules/antd/dist/antd.js'),
-      '@ant-design/icons': path.join(__dirname,'node_modules/@ant-design/icons/dist/index.umd.js'),
-    }
-  }
-})
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@utils': path.resolve(__dirname, 'src/utils'),
+			'@components': path.resolve(__dirname, 'src/components'),
+			antd: path.join(__dirname, 'node_modules/antd/dist/antd.js'),
+			'@ant-design/icons': path.join(__dirname, 'node_modules/@ant-design/icons/dist/index.umd.js'),
+		},
+	},
+});
