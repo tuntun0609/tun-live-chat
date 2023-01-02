63db40d8b295de0c62498a7dbee23faabb2837a5
tuntun <760523867@qq.com>
2023-01-02 15:38:13 +0800

    feat: 粉丝徽章增加颜色渐变和舰队图标展示

--- src/components/danmu-item/danmu-item.tsx
+++ src/components/danmu-item/danmu-item.tsx
@@ -1,4 +1,8 @@
 import { useMemo } from 'react';
+
+import ship1Icon from '../../assets/ship-1.png';
+import ship2Icon from '../../assets/ship-2.png';
+import ship3Icon from '../../assets/ship-3.png';
 import './danmu-item.scss';
 
 export enum DanmuType {
@@ -8,25 +12,46 @@ export enum DanmuType {
 
 // 粉丝勋章
 const FansMedal = ({data}: {data: any}) => {
-	const color = useMemo(() => dec2hex(data[4]), []);
+	const color1 = useMemo(() => dec2hex(data[4]), []);
+	const color2 = useMemo(() => dec2hex(data[7]), []);
+	const color3 = useMemo(() => dec2hex(data[9]), []);
+	const getShipIcon = (level: number) => {
+		switch (level) {
+		case 1:
+			return ship1Icon;
+		case 2:
+			return ship2Icon;
+		case 3:
+			return ship3Icon;
+		default:
+			return '';
+		}
+	};
 	return (
 		<div className='fans-medal'
 			style={{
-				borderColor: `#${color}`,
+				borderColor: `#${color2}`,
 			}}
 		>
 			<div
 				className='fans-medal-name'
 				style={{
-					backgroundColor: `#${color}`,
+					backgroundImage: `linear-gradient(45deg, #${color1}, #${color3})`,
 				}}
 			>
+				{
+					data[10] === 1 || data[10] === 2 || data[10] === 3
+						? <i className='fans-medal-ship-icon' style={{
+							backgroundImage: `url(${getShipIcon(data[10])})`,
+						}}></i>
+						: null
+				}
 				{data[1]}
 			</div>
 			<div
 				className='fans-medal-level'
 				style={{
-					color: `#${color}`,
+					color: `#${color1}`,
 				}}
 			>
 				{data[0]}
@@ -51,7 +76,9 @@ export const DanmuItem = (props: {danmuData: DanmuItem}) => {
 		return (
 			<div data-id={danmuData.key} className='danmu-item danmu-msg'>
 				{
-					danmuData.isFansMedal === 'true' && danmuData.data[3].length !== 0
+					danmuData.isFansMedal === 'true'
+					&& danmuData.data[3]?.length !== 0
+					&& danmuData.data[3]?.[11] === 1
 						? <FansMedal data={danmuData.data[3]}></FansMedal>
 						: null
 				}
