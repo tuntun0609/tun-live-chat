3ffb6e292d74e4b37d5953e741a220ff5a2359d5
tuntun <760523867@qq.com>
2023-01-08 16:59:37 +0800

    refactor: 将默认弹幕速度修改为10

--- src/utils/DEFAULT_VALUES.ts
+++ src/utils/DEFAULT_VALUES.ts
@@ -1,4 +1,4 @@
 export const DEFAULT_REMOVE_DELAY = 300;
 export const DEFAULT_OFFSET = 30;
-export const DEFAULT_DANMU_SPEED = 5;
+export const DEFAULT_DANMU_SPEED = 10;
 export const DEFAULT_NAME_COLOR = '#fb7299';
