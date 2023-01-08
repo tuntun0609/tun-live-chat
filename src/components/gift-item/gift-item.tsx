526711eac834229de8c8135a08f55554ab4f2d36
tuntun <760523867@qq.com>
2023-01-08 17:28:44 +0800

    feat: 礼物弹幕增加价格提示

--- src/components/gift-item/gift-item.tsx
+++ src/components/gift-item/gift-item.tsx
@@ -10,18 +10,23 @@ export const GiftItem = ({data}: {data: DanmuItem}) => (
 		className='danmu-item danmu-gift'
 	>
 		<div className='danmu-gift-header'>
-			{
-				data.setting?.isFansMedal === 'true'
-					&& !isEmpty(data.data?.medal_info ?? {})
-					? <FansMedal data={fansMedalDataTran(data.data?.medal_info ?? {})}></FansMedal>
-					: null
-			}
-			<Avatar
-				className='danmu-gift-header-face'
-				src={data.data?.face}
-			></Avatar>
-			<div className='danmu-gift-header-name'>
-				{data.data?.uname}
+			<div className='danmu-gift-header-info'>
+				{
+					data.setting?.isFansMedal === 'true'
+						&& !isEmpty(data.data?.medal_info ?? {})
+						? <FansMedal data={fansMedalDataTran(data.data?.medal_info ?? {})}></FansMedal>
+						: null
+				}
+				<Avatar
+					className='danmu-gift-header-face'
+					src={data.data?.face}
+				></Avatar>
+				<div className='danmu-gift-header-name'>
+					{data.data?.uname}
+				</div>
+			</div>
+			<div className='danmu-gift-header-price'>
+				{data.data?.price / 1000}
 			</div>
 		</div>
 		<div className='danmu-gift-content'>
